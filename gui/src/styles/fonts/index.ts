c215522863fa405661c7a4e68e6f23b59eb22fab
Gabriel Victor <gabrielvictorweb@outlook.com>
2023-09-24 22:21:11 -0300

    chore: import ttf

---
+++ gui/src/styles/fonts/index.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+import SchrottingerJustusSchrotte from '../../assets/fonts/Schrottinger_Justus_Schrotte.ttf';
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'SchrottingerJustusSchrotte';
+        src: url(${SchrottingerJustusSchrotte}) format('truetype');
+    }
+`;
