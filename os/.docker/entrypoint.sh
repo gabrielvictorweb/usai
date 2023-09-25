9ae21ac2aaf749a6b012be8665b735dd3d496516
Gabriel Victor <gabrielvictorweb@outlook.com>
2023-09-24 22:56:13 -0300

    fix: running java and python apps at the same time

--- os/.docker/entrypoint.sh
+++ os/.docker/entrypoint.sh
@@ -3,9 +3,10 @@
 # execute migrations, create user admin and run server (Django) 
 python3 python/manage.py migrate
 python3 python/manage.py createsuperuser --email admin@example.com --username admin
-python3 python/manage.py runserver 0.0.0.0:8000
 
 # compile and generate package .jar to run server (Maven)
 (cd ./java/ && mvn compile)
 (cd ./java/ && mvn package)
-(cd ./java/ && java -jar target/restapi-0.0.1-SNAPSHOT.jar)
+
+# run servers
+python3 python/manage.py runserver 0.0.0.0:8000 & (cd ./java/ && java -jar target/restapi-0.0.1-SNAPSHOT.jar)
